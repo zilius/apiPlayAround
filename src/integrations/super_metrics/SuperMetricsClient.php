3315ba07ec80d295dee038d7bc3ab834387c45d3
Zygimantas <zygimantas.zilevicius@gmail.com>
2020-04-29 23:40:30 +0300

    Initial commit

---
+++ src/integrations/super_metrics/SuperMetricsClient.php
@@ -0,0 +1,18 @@
+<?php
+
+
+use App\Interfaces\BaseClient;
+
+class SuperMetricsClient implements BaseClient
+{
+
+    public function get()
+    {
+        // TODO: Implement get() method.
+    }
+
+    public function post()
+    {
+        // TODO: Implement post() method.
+    }
+}
---
+++ src/integrations/super_metrics/SuperMetricsService.php
@@ -0,0 +1,23 @@
+<?php
+
+
+use App\Interfaces\BaseClient;
+
+class SuperMetricsService
+{
+    /**
+     * @var BaseClient
+     */
+    private $client;
+
+    public function __construct(BaseClient $client)
+    {
+
+        $this->client = $client;
+    }
+
+    public function registerToken(string $email, string $name): string
+    {
+
+    }
+}
